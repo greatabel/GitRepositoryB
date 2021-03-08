9f37504291c2050a0ca76bf03d1f134b0b298c43
greatabel <myreceiver2for2github@gmail.com>
2021-03-09 02:25:09 +0800

    test

---
+++ test.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
+ax.plot([0,1,2], [10,20,3])
+fig.savefig('to.png')   # save the figure to file
+plt.close(fig)    # close the figure window
